b611b5b7a38c89578f90181af161c83b371265bf
Sunil Nathyogi <sunilnathyogi008@gmail.com>
2024-03-19 09:20:00 +0530

    feat: add default layout with preloader, header and cursor

---
+++ layouts/default.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="__nuxt">
+    <ThePreloader />
+    <TheHeader />
+    <div class="__page">
+      <slot />
+    </div>
+    <TheCursor />
+  </div>
+</template>
+
+<script setup lang="ts">
+import ThePreloader from "~/components/ThePreloader.vue";
+import TheHeader from "~/components/TheHeader.vue";
+import TheCursor from "~/components/TheCursor.vue";
+</script>
