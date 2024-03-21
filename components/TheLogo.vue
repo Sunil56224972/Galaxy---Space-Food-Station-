889010b77e3b6431c4019b31abbdc409e2edb80a
Sunil Nathyogi <sunilnathyogi008@gmail.com>
2024-03-21 09:00:00 +0530

    feat: build TheLogo component

---
+++ components/TheLogo.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="the-logo">
+    <img src="/images/logo.svg" alt="Planetoño – Space Food Station" width="350" height="106" />
+  </div>
+</template>
+
+<style scoped>
+.the-logo {
+  align-items: center;
+  display: flex;
+  justify-content: center;
+  width: 100%;
+}
+.the-logo img {
+  height: auto;
+  width: 100%;
+  will-change: transform;
+}
+</style>
