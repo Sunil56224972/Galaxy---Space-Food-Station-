4942eee34f4d9e8e2f242c73aceecf8bb9577a71
Sunil Nathyogi <sunilnathyogi008@gmail.com>
2024-03-28 15:00:00 +0530

    feat: compose TheHeader switching desktop and mobile variants

---
+++ components/TheHeader.vue
@@ -0,0 +1,27 @@
+<template>
+  <header class="the-header">
+    <TheHeaderDesktop />
+    <TheHeaderMobile />
+  </header>
+</template>
+
+<script setup lang="ts">
+import TheHeaderDesktop from "~/components/TheHeaderDesktop.vue";
+import TheHeaderMobile from "~/components/TheHeaderMobile.vue";
+</script>
+
+<style scoped>
+.the-header {
+  left: 0;
+  position: fixed;
+  top: 0;
+  width: 100%;
+  z-index: 4;
+}
+@media only screen and (max-width: 900px) {
+  .the-header :deep(.the-header-desktop) { display: none; }
+}
+@media only screen and (min-width: 901px) {
+  .the-header :deep(.the-header-mobile) { display: none; }
+}
+</style>
