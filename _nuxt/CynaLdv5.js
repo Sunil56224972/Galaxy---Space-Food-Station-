af10d1ba86fa6e98c8b4b4343fa8e9c82e689783
Sunil Nathyogi <sunilnathyogi008@gmail.com>
2024-05-28 16:00:00 +0530

    chore: generate production static export to _nuxt/

---
+++ _nuxt/CynaLdv5.js
@@ -0,0 +1,2 @@
+import{s as t,z as n}from"#entry";function s(){const e=()=>{const o=["background: #0e0e0e","color: #fff","padding: 24px 16px"].join(";");console.log("%c Coded with ♥️ by Sunil Nathyogi: https://linkedin.com/in/sunil-81340839a",o)};t(()=>{e()})}const r={__name:"default",setup(e){return s(),(o,u)=>n(o.$slots,"default")}};export{r as default};
+
